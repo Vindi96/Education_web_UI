a2373e406334e31a21f53b80f9136785c1bb5393
vindi96 <vindya@silverstripers.com>
2024-05-24 18:28:04 +0530

    added aniations

--- main.js
+++ main.js
@@ -41,3 +41,30 @@ close_button.addEventListener('click',()=>{
     close_button.style.display='none';
 })
 
+const fades = document.querySelectorAll('[data-fadein]');
+    function checkLoaded() {
+        const w = window.innerHeight;
+        const offset = 100;
+        for (const dom of fades) {
+            const rect = dom.getBoundingClientRect();
+            if (rect.y < w - offset) {
+                dom.classList.add('active');
+            } else {
+                if (parseInt(dom.getAttribute('data-once')) !== 1) {
+                    dom.classList.remove('active');
+                }
+            }
+        }
+    }
+
+
+    window.addEventListener("load", () => {
+        window.setTimeout(() => {
+            if (fades.length) {
+                checkLoaded();
+                window.addEventListener('scroll', () => {
+                    checkLoaded()
+                });
+            }
+        }, 500);
+    });
